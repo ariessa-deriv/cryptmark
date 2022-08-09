0c79cbbc5d5e7ea57369867608b66504ace42e70
ariessa-deriv <>
2022-08-09 10:50:01 +0800

    Add search bar widget

---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  final TextEditingController textController = TextEditingController();
+  final String hintText = 'Search for a coin...';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            offset: const Offset(12, 26),
+            blurRadius: 50,
+            spreadRadius: 0,
+            color: Colors.grey.withOpacity(.1)),
+      ]),
+      child: TextField(
+        controller: textController,
+        onChanged: (value) {},
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.search,
+            color: Colors.grey,
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          hintText: hintText,
+          hintStyle: const TextStyle(color: Colors.grey),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey, width: 2.0),
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
